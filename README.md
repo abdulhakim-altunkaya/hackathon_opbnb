d7ff8ec92787e6cc7a969d3be16043e38a788299
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-07-30 00:36:15 +0300

    project started, dependencies downloaded, cleanup done

---
+++ README.md
@@ -0,0 +1,10 @@
+# FoggyBank: Anonymous Transactions Platform
+
+Author: Abdulhakim Altunkaya
+Start Date: 30 July 2023
+
+## Credits
+
+Logo [Looka website](https://looka.com/)
+
+
---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+
+const hre = require("hardhat");
+
+async function main() {
+
+
+  const foggyBank = await hre.ethers.deployContract("FoggyBank", [], {});
+
+  await foggyBank.waitForDeployment();
+
+  console.log(`foggyBank deployed to ${foggyBank.target}`);
+}
+
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
