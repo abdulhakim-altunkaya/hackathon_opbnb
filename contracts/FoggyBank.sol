25c23375720362aef35d03057444fa8d8f7530da
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-08-15 11:43:16 +0300

    depossit token function is finisshed

--- contracts/FoggyBank.sol
+++ contracts/FoggyBank.sol
@@ -11,13 +11,18 @@ contract FoggyBank is Ownable {
     event Deposit(address indexed depositor, uint amount);
     event Withdraw(address indexed receiver, uint amount);
 
-    //SETTING POOL TOKEN: TokenA
-    IERC20 public tokenContract;
-    function setToken(address tokenAddress) external {
-        tokenContract = IERC20(tokenAddress);
+    //************SETTING POOL TOKEN: TokenA************
+    IERC20 public tokenAContract;
+    function setTokenA(address _tokenAddress) external {
+        tokenAContract = IERC20(_tokenAddress);
+    }
+    //************SETTING FOGGY TOKEN************
+    IERC20 public tokenFoggyContract;
+    function setTokenFoggy(address _tokenAddress) external {
+        tokenFoggyContract = IERC20(_tokenAddress);
     }
 
-    //STATE VARIABLES
+    //************STATE VARIABLES*************
     //each deposit will have a hash and an amount information
     mapping(bytes32 => uint) private balances;
     //these hashes will be saved in an array
@@ -25,7 +30,8 @@ contract FoggyBank is Ownable {
     //there will be a fee for calling deposit and withdraw functions to deter scammers
     mapping(address => bool) public feePayers;
 
-    //SECURITY 1: pause contract
+
+    //----------------SECURITY 1: pause contract
     bool public status;
     error Stopped(string message, address owner);
     modifier isPaused() {
@@ -38,7 +44,7 @@ contract FoggyBank is Ownable {
         status = !status;
     }
 
-    //SECURITY 2: checking if new hash already exists
+    //--------------SECURITY 2: checking if new hash already exists
     error Existing(string message, bytes32 hashdata);
     modifier isExisting(bytes32 _hash) {
         for (uint256 i = 0; i < balanceIds.length; i++) {
@@ -49,7 +55,7 @@ contract FoggyBank is Ownable {
         _;
     }
 
-    //SECURITY 3: checking if msg.sender has paid function call fee
+    //---------------SECURITY 3: checking if msg.sender has paid function call fee
     error NotPaid(string message, address caller);
     modifier hasPaid() {
         if (feePayers[msg.sender] == false) {
@@ -58,4 +64,59 @@ contract FoggyBank is Ownable {
         _;
     }
 
+    //**************SETTING FEE********************** 
+    //1 is an arbitrary value, ether is for handling decimals. Fee is in Foggy
+    //3.PAYMENT OPERATIONS
+    uint public fee = 1;
+    function setFee(uint _fee) external onlyOwner {
+        fee = _fee;
+    }
+    function makePayment() external returns(bool) {
+        require(balanceOf(msg.sender) >= fee, "you don't have CONTOR");
+        require(msg.sender == tx.origin, "contracts cannot withdraw");
+        require(msg.sender != address(0), "real addresses can withdraw");
+        _transfer(msg.sender, address(this), fee*(10**18));
+    }
+
+    //SERVICE FEE
+    uint public fee = 1;
+    function setFee(uint _fee) external onlyOwner {
+        fee = _fee;
+    }
+    //owner can collect FOGGY tokens inside the contract
+    function collectFees() external onlyOwner {
+        uint balanceFoggy = tokenFoggyContract.balanceOf(address(this));
+        if (balanceFoggy > 0) {
+           tokenFoggyContract._transfer(address(this), msg.sender, balanceFoggy);
+        }
+    }
+    //People must pay service fee for each withdrawal and deposit operation
+    function payFee() public {
+        require(tokenFoggyContract.balanceOf(msg.sender) >= fee*(10**18), "you don't have FOGGY");
+        require(msg.sender == tx.origin, "contracts cannot withdraw");
+        require(msg.sender != address(0), "real addresses can withdraw");
+        tokenFoggyContract._transfer(msg.sender, address(this), fee*(10**18));
+        feePayers[msg.sender] == true;
+    }
+
+    // ------------------------------------------------------------------------
+    //                          DEPOSIT AND WITHDRAWAL FUNCTIONS
+    // ------------------------------------------------------------------------
+
+    //Function to deposit tokens into the contract, decimals handled inside the function
+    function deposit(bytes32 _hash, uint _amount) external hasPaid isExisting(_hash) isPaused {
+        //input validations
+        require(_hash.length == 32, "invalid hash");
+        require(_amount >= 1, "_amount must be bigger than 1");
+        //general checks
+        require(msg.sender == tx.origin, "contracts cannot withdraw");
+        require(msg.sender != address(0), "real addresses can withdraw");
+        require(tokenAContract.balanceOf(msg.sender) >= 0, "you don't have TokenA");
+        //operations
+        tokenAContract._transfer(msg.sender, address(this), _amount*(10**18));
+        feePayers[msg.sender] = false;
+        balanceIds.push(_hash);
+        uint amount = _amount*(10**18);
+        balances[_hash] = amount;
+    }
 }
